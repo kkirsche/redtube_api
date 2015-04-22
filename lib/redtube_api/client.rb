4f7a457d76cdd16b96e6ce1efae9f2f82eba1a41
Kevin Kirsche <Kev.Kirsche@gmail.com>
2015-04-21 22:30:13 -0400

    Add optional validation

--- lib/redtube_api/client.rb
+++ lib/redtube_api/client.rb
@@ -10,13 +10,70 @@ module RedtubeApi
       self
     end
 
-    def get(type, args, symbols)
+    private
+    def validate(response)
+      return true if response['code'].nil? && response[:code].nil?
+
+      error_codes = [1001, 1002, 1003, 2001, 2002, 3001, 3002, 3003]
+
+      fail 'Unknown error code. Please contact the developer at' \
+        ' https://github.com/kkirsche/redtube_api/issues' \
+        unless error_codes.include? response['code'].to_i || error_codes.include? response[:code].to_i
+
+      fail 'No such method. Please contact the developer at' \
+        ' https://github.com/kkirsche/redtube_api/issues' \
+        if response['code'] == 1001 || response[:code] == 1001
+
+      fail 'No such data provider. Please contact the developer at' \
+        ' https://github.com/kkirsche/redtube_api/issues' \
+        if response['code'] == 1002 || response[:code] == 1002
+
+      fail 'No input parameters specified. Please contact the developer at' \
+        ' https://github.com/kkirsche/redtube_api/issues' \
+        if response['code'] == 1003 || response[:code] == 1003
+
+      fail 'No videos found.' \
+        if response['code'] == 2001 || response[:code] == 2001
+
+      fail 'No video with this ID.' \
+        if response['code'] == 2002 || response[:code] == 2002
+
+      fail 'No categories found.' \
+        if response['code'] == 3001 || response[:code] == 3001
+
+      fail 'No tags found.' \
+        if response['code'] == 3002 || response[:code] == 3002
+
+      fail 'No stars found.' \
+        if response['code'] == 3003 || response[:code] == 3003
+    end
+
+    def get(type, args, symbols, should_validate)
       request_args = symbols.map { |sym| args[sym] unless args[sym.nil?] }
       request_args[:data] = type
       request_args[:output] = 'json'
-      @client.get('', request_args)
+      response = JSON.parse(@client.get('', request_args).body)
+      validate response unless should_validate
+
     end
 
+    def receive_should_validate?(args)
+      args[:should_validate] = true if args[:should_validate].nil?
+    end
+
+    def receive_video_id?(args)
+      fail ':video_id was either not detected or was not of type Integer.' \
+           'Please provide an integer :video_id.' unless args[:video_id].is_a? Integer
+    end
+
+    def receive_valid_thumbsize?(args)
+      fail 'Invalid thumbsize. Possible values are small, medium, medium1,' \
+           ' medium2, big, or all.' unless
+           ['medium', 'small', 'big', 'all', 'medium1',
+            'medium2'].include? args[:thumbsize]
+    end
+
+    public
     # == Search Videos
     # The Search Videos API endpoint allows us to search for a video of your choice.
     #
@@ -25,9 +82,11 @@ module RedtubeApi
     #    videos = RedtubeApi::Client.new.search_videos category: 'Blowjobs', page: 2
     #
     def search_videos(args = {})
+      receive_should_validate? args
+
       get('redtube.Videos.searchVideos', args,
         [:category, :page, :search, :tags, :stars,
-         :thumbsize, :ordering, :period])
+         :thumbsize, :ordering, :period], args[:should_validate])
     end
 
     # == Get Video By ID
@@ -38,14 +97,12 @@ module RedtubeApi
     #    videos = RedtubeApi::Client.new.get_video_by_id video_id: '179543', thumbsize: 'big'
     #
     def get_video_by_id(args = {})
-      fail ':video_id was either not detected or was not of type Integer.' \
-        'Please provide an integer :video_id.' unless args[:video_id].is_a? Integer
-      fail 'Invalid thumbsize. Possible values are small, medium, medium1,' \
-           ' medium2, big, or all.' unless
-           ['medium', 'small', 'big', 'all', 'medium1',
-            'medium2'].include? args[:thumbsize]
+      receive_video_id? args
+      receive_valid_thumbsize? args
+      receive_should_validate? args
+
       get('redtube.Videos.getVideoById', args,
-        [:video_id, :thumbsize])
+        [:video_id, :thumbsize], args[:should_validate])
     end
 
     # == Is Video Active
@@ -56,10 +113,11 @@ module RedtubeApi
     #    videos = RedtubeApi::Client.new.is_video_active video_id: '179543'
     #
     def is_video_active(args = {})
-      fail ':video_id was either not detected or was not of type Integer.' \
-        'Please provide an integer :video_id.' unless args[:video_id].is_a? Integer
+      receive_video_id? args
+      receive_should_validate? args
+
       get('redtube.Videos.isVideoActive', args,
-        [:video_id])
+        [:video_id], args[:should_validate])
     end
 
     # == Get Video Embed Code
@@ -70,10 +128,11 @@ module RedtubeApi
     #    videos = RedtubeApi::Client.new.get_video_embed_code video_id: '179543'
     #
     def get_video_embed_code(args = {})
-      fail ':video_id was either not detected or was not of type Integer.' \
-        'Please provide an integer :video_id.' unless args[:video_id].is_a? Integer
+      receive_video_id? args
+      receive_should_validate? args
+
       get('redtube.Videos.getVideoEmbedCode', args,
-        [:video_id])
+          [:video_id], args[:should_validate])
     end
 
     # == Get Deleted Videos
@@ -86,8 +145,59 @@ module RedtubeApi
     def get_deleted_videos(args = {})
       fail ':page was either not detected or was not of type Integer.' \
       'Please provide an integer :page.' unless args[:page].is_a? Integer
+
+      receive_should_validate? args
+
       get('redtube.Videos.getDeletedVideos', args,
-        [:page])
+          [:page], args[:should_validate])
+    end
+
+    # == Get Categories List
+    # Retrieves all categories available.
+    #
+    # ==== Example
+    #
+    #    videos = RedtubeApi::Client.new.get_categories_list
+    #
+    def get_categories_list
+      receive_should_validate? args
+      get('redtube.Categories.getCategoriesList', {}, [], args[:should_validate])
+    end
+
+    # == Get Tag List
+    # Retrieves all tags available.
+    #
+    # ==== Example
+    #
+    #    videos = RedtubeApi::Client.new.get_tag_list
+    #
+    def get_tag_list
+      receive_should_validate? args
+      get('redtube.Tags.getTagList', {}, [], args[:should_validate])
+    end
+
+    # == Get Star List
+    # Retrieves all pornstars available.
+    #
+    # ==== Example
+    #
+    #    videos = RedtubeApi::Client.new.get_star_list
+    #
+    def get_star_list
+      receive_should_validate? args
+      get('redtube.Stars.getStarList', {}, [], args[:should_validate])
+    end
+
+    # == Get Detailed Star List
+    # Retrieves all pornstars available with details (page url and star's thumb).
+    #
+    # ==== Example
+    #
+    #    videos = RedtubeApi::Client.new.get_detailed_star_list
+    #
+    def get_detailed_star_list
+      receive_should_validate? args
+      get('redtube.Stars.getStarDetailedList', {}, [], args[:should_validate])
     end
   end
 end
